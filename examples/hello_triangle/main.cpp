1113c3dd61cbfc2ca0fa539fb40022b4373856df
Stuart Millman <stu.millman15@gmail.com>
2023-05-12 18:43:53 +0100

    Add hello_triangle example (still needs to be implemented!).

---
+++ examples/hello_triangle/main.cpp
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) Stuart Millman 2023.
+ */
+
+#include "gfx.hpp"
+
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+
+using namespace sm;
+
+auto read_shader_file(const char* filename) -> std::vector<char>
+{
+	if (std::ifstream file{ filename, std::ios::binary | std::ios::ate })
+	{
+		const std::streamsize fileSize = file.tellg();
+		file.seekg(0);
+		std::vector<char> shaderBinary(fileSize);
+		file.read(shaderBinary.data(), fileSize);
+		return shaderBinary;
+	}
+
+	GFX_LOG_ERR_FMT("Example - compute - Failed to read shader file: {}", filename);
+	return {};
+}
+
+int main()
+{
+	glfwInit();
+	auto window = glfwCreateWindow(1080, 720, "Hello Triangle", nullptr, nullptr);
+
+	gfx::set_error_callback([](const char* msg) {
+		GFX_LOG_ERR(msg);
+		GFX_ASSERT(false, "");
+	});
+
+	gfx::AppInfo appInfo{ "Hello Triangle App" };
+	if (!gfx::initialise(appInfo))
+	{
+		throw std::runtime_error("Failed to initialise GFX!");
+	}
+
+	gfx::DeviceInfo device_info{
+		.deviceFlags = gfx::DeviceFlags_PreferDiscrete,
+		.queueFlags = { gfx::QueueFlags_Graphics },
+	};
+	gfx::DeviceHandle deviceHandle{};
+	if (!gfx::create_device(deviceHandle, device_info))
+	{
+		throw std::runtime_error("Failed to create GFX device!");
+	}
+
+	const auto shaderBinary = read_shader_file("triangle.spv");
+	gfx::ComputePipelineInfo pipelineInfo{
+		.shaderCode = shaderBinary,
+		.descriptorSets = {
+			gfx::DescriptorSetInfo{ .bindings = {
+										{ gfx::DescriptorType::eStorageBuffer, 1, gfx::ShaderStageFlags_Compute },
+										{ gfx::DescriptorType::eStorageBuffer, 1, gfx::ShaderStageFlags_Compute },
+									} },
+		},
+	};
+	gfx::PipelineHandle pipelineHandle{};
+	if (!gfx::create_compute_pipeline(pipelineHandle, deviceHandle, pipelineInfo))
+	{
+		throw std::runtime_error("Failed to create GFX compute pipeline!");
+	}
+
+	gfx::CommandListHandle commandListHandle{};
+	if (!gfx::create_command_list(commandListHandle, deviceHandle, 0))
+	{
+		throw std::runtime_error("Failed to create GFX command list!");
+	}
+
+	while (glfwWindowShouldClose(window) == 0)
+	{
+		glfwPollEvents();
+
+		gfx::reset(commandListHandle);
+		gfx::begin(commandListHandle);
+		gfx::bind_pipeline(commandListHandle, pipelineHandle);
+		gfx::end(commandListHandle);
+
+		gfx::SubmitInfo submitInfo{
+			.commandList = commandListHandle,
+			.waitSemaphoreHandle = {}
+		};
+		gfx::FenceHandle fenceHandle;
+		gfx::submit_command_list(submitInfo, &fenceHandle, nullptr);
+
+		gfx::wait_on_fence(fenceHandle);
+	}
+
+	gfx::destroy_device(deviceHandle);
+	gfx::shutdown();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
